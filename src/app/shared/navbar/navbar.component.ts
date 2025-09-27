b0bdfd856bb1077002a941a628e56714bfd744c4
Angel Gaitan <angel.gaitan.ev@gmail.com>
2025-09-27 00:25:57 -0300

    added login modal

--- src/app/shared/navbar/navbar.component.ts
+++ src/app/shared/navbar/navbar.component.ts
@@ -1,26 +1,39 @@
 import { Component, HostListener } from '@angular/core';
 import { RouterLink, RouterLinkActive } from '@angular/router';
 import { ButtonLanguagesComponent } from '../button-languages/button-languages.component';
+import { CommonModule } from '@angular/common';
 
 @Component({
   selector: 'app-navbar',
   standalone: true,
-  imports: [RouterLink, RouterLinkActive, ButtonLanguagesComponent],
+  imports: [RouterLink, RouterLinkActive, ButtonLanguagesComponent, CommonModule],
   templateUrl: './navbar.component.html',
   styleUrl: './navbar.component.scss'
 })
 export class NavbarComponent {
   isMenuOpen = false;
   isScrolled = false;
+  clickCount = 0;
+  showLoginModal = false;
 
   @HostListener('window:scroll')
   onWindowScroll() {
-    console.log('Scroll position:', window.scrollY);
     this.isScrolled = window.scrollY > 10;
-    console.log('isScrolled:', this.isScrolled);
   }
 
   toggleMenu() {
     this.isMenuOpen = !this.isMenuOpen;
+  }
+
+  onTitleClick() {
+    this.clickCount++;
+    if (this.clickCount >= 4) {
+      this.showLoginModal = true;
+      this.clickCount = 0; // reinicia para futuros usos
+    }
+  }
+
+  closeModal() {
+    this.showLoginModal = false;
   }
 }
